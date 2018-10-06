407787c4082bab44b1a835168a37575502dcb4b8
Kai Krakow <kai@kaishome.de>
2018-10-06 03:48:22 +0200

    [local] Use triple-buffering by default
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- src/d3d11/d3d11_options.cpp
+++ src/d3d11/d3d11_options.cpp
@@ -11,7 +11,7 @@ namespace dxvk {
     this->maxTessFactor         = config.getOption<int32_t>("d3d11.maxTessFactor",      0);
     this->samplerAnisotropy     = config.getOption<int32_t>("d3d11.samplerAnisotropy",  -1);
     this->deferSurfaceCreation  = config.getOption<bool>("dxgi.deferSurfaceCreation",   false);
-    this->numBackBuffers        = config.getOption<int32_t>("dxgi.numBackBuffers", 0);
+    this->numBackBuffers        = config.getOption<int32_t>("dxgi.numBackBuffers", 3);
     this->syncInterval          = config.getOption<int32_t>("dxgi.syncInterval", -1);
     this->syncMode              = D3D11SwapChainSyncMode(config.getOption<int32_t>("dxgi.syncMode", 0));
   }
